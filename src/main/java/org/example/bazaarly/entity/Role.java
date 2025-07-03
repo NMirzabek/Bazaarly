c8fc0ed7411332369c2d4e3b8e18e03332220f06
mirza <mirzabeknormakhmatov03@gmail.com>
2025-07-03 20:47:32 +0500

    Built security

--- src/main/java/org/example/bazaarly/entity/Role.java
+++ src/main/java/org/example/bazaarly/entity/Role.java
@@ -6,12 +6,20 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.example.bazaarly.entity.base.BaseEntity;
+import org.springframework.security.core.GrantedAuthority;
 
 @Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Table(name = "roles")
-public class Role extends BaseEntity {
+public class Role extends BaseEntity implements GrantedAuthority {
+
     private String name;
+
+    @Override
+    public String getAuthority() {
+        return this.name;
+    }
+
 }
--- src/main/java/org/example/bazaarly/entity/User.java
+++ src/main/java/org/example/bazaarly/entity/User.java
@@ -8,8 +8,11 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.example.bazaarly.entity.base.BaseEntity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 
+import java.util.Collection;
 import java.util.List;
 
 @Entity
@@ -17,13 +20,28 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 @Table(name = "users")
-public class User extends BaseEntity {
+public class User extends BaseEntity implements UserDetails {
 
     private String firstName;
     private String lastName;
     private String email;
     private String password;
 
+    public User(String email, List<Role> roles) {
+        this.email = email;
+        this.roles = roles;
+    }
+
     @ManyToMany(fetch = FetchType.EAGER)
     private List<Role> roles;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.roles;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
 }

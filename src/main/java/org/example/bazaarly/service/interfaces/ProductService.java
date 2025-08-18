c2a1d96026e134cfb034aed0b67ff4e62bc18116
mirza <mirzabeknormakhmatov03@gmail.com>
2025-08-18 12:06:11 +0500

    add product module with DTO, service, repository

--- src/main/java/org/example/bazaarly/service/interfaces/ProductService.java
+++ src/main/java/org/example/bazaarly/service/interfaces/ProductService.java
@@ -1,12 +1,13 @@
 package org.example.bazaarly.service.interfaces;
 
+import org.example.bazaarly.dto.projections.ProductProjection;
 import org.example.bazaarly.dto.request.ProductDTO;
 import org.example.bazaarly.entity.Product;
 
 import java.util.List;
 
 public interface ProductService {
-    List<Product> getAll();
+    List<ProductProjection> getAll();
 
     Product save(ProductDTO productDTO);
 }
